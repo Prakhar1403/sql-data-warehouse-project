264cddd62a6943bf3800011f0f00859449a8d5d6
Prakhar Shukla <118876452+Prakhar1403@users.noreply.github.com>
2025-09-14 22:10:33 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+use master
+
+--Creating Database 'DataWarehouse'
+
+create database Datawarehouse;
+
+use Datawarehouse
+
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
